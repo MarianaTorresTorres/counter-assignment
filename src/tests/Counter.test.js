39a58a4d86051b8d1d64b81d7c6bdb763a104e7d
MarianaTorresTorres <mari.torret@gmail.com>
2024-04-09 23:03:16 -0400

    all tests pass

--- src/tests/Counter.test.js
+++ src/tests/Counter.test.js
@@ -1,22 +1,37 @@
-// import necessary react testing library helpers here
-// import the Counter component here
+import { render, screen, fireEvent } from "@testing-library/react";
+import Counter from "../components/Counter";
 
 beforeEach(() => {
-  // Render the Counter component here
-})
-
-test('renders counter message', () => {
-  // Complete the unit test below based on the objective in the line above
+  render(<Counter />);
 });
 
-test('should render initial count with value of 0', () => {
-  // Complete the unit test below based on the objective in the line above
+test("renders counter message", () => {
+  const counterMsg = screen.getByText(/Counter/i);
+  expect(counterMsg).toBeInTheDocument();
 });
 
-test('clicking + increments the count', () => {
-  // Complete the unit test below based on the objective in the line above
+test("should render initial count with value of 0", () => {
+  const countValue = screen.getByTestId("count");
+  expect(countValue).toBeInTheDocument();
+  expect(countValue.textContent).toBe("0");
 });
 
-test('clicking - decrements the count', () => {
-  // Complete the unit test below based on the objective in the line above
+test("clicking + increments the count", () => {
+  const countValue = screen.getByTestId("count");
+  expect(countValue).toBeInTheDocument();
+  expect(countValue.textContent).toBe("0");
+  const plusButton = screen.getByRole("button", { name: "+" });
+  fireEvent.click(plusButton);
+  expect(countValue).toBeInTheDocument();
+  expect(countValue.textContent).toBe("1");
+});
+
+test("clicking - decrements the count", () => {
+  const countValue = screen.getByTestId("count");
+  expect(countValue).toBeInTheDocument();
+  expect(countValue.textContent).toBe("0");
+  const minusButton = screen.getByRole("button", { name: "-" });
+  fireEvent.click(minusButton);
+  expect(countValue).toBeInTheDocument();
+  expect(countValue.textContent).toBe("-1");
 });
